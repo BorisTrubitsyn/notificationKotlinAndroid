c0228c20517d32fce44d7997286684e693740ab1
Boris <borislogintrub@gmail.com>
2020-11-30 12:56:46 +0200

    send notification with open activity

--- app/src/main/java/com/example/notificationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/notificationkotlin/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.notificationkotlin
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+
 
 class MainActivity : AppCompatActivity() {
     private val NOTIFY_ID = 101
@@ -16,12 +19,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btn = findViewById<Button>(R.id.btn)
+
+
         btn.setOnClickListener {
+            //open main activity
+            val notificationIntent = Intent(this@MainActivity, MainActivity::class.java)
+            val contentIntent = PendingIntent.getActivity(this@MainActivity,
+                    0, notificationIntent,
+                    PendingIntent.FLAG_CANCEL_CURRENT)
+            //send notification
             val builder = NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_launcher_background)
                     .setContentTitle("Напоминание")
                     .setContentText("Пора покормить кота")
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(contentIntent)
             val notificationManager = NotificationManagerCompat.from(this@MainActivity)
             notificationManager.notify(NOTIFY_ID, builder.build())
         }
